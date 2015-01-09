f1d515ce94de9e1a046791d776e0501ec1c235e1
Bruce Becker <brucellino@gmail.com>
2015-01-10 00:21:34 +0100

    added build check script

---
+++ check-build.sh
@@ -0,0 +1,28 @@
+cd $WORKSPACE/$NAME-$VERSION
+make check
+make install DESTDIR=$WORKSPACE/build
+
+mkdir -p $REPO_DIR
+rm -rf $REPO_DIR/*
+tar -cvzf $REPO_DIR/build.tar.gz -C $WORKSPACE/build apprepo
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       GMP_VERSION       $VERSION
+setenv       GMP_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME
