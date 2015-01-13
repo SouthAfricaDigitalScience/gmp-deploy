3057bf9a40cadbab209fd1f72c76f5d23c8be3d4
Bruce Becker <brucellino@gmail.com>
2015-01-13 13:28:45 +0100

    removed the subdirectory for the -, which is encoded in the directory structure

--- build.sh
+++ build.sh
@@ -16,13 +16,16 @@ mkdir -p $WORKSPACE
 mkdir -p $SRC_DIR
 mkdir -p $SOFT_DIR
 
+#  Download the source file
 
 if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
+  echo "seems like this is the first build - let's get the source"
   mkdir -p $SRC_DIR
   wget http://mirror.ufs.ac.za/gnu/gnu/gmp/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
 else
+  echo "continuing from previous builds, using source at " $SRC_DIR/$SOURCE_FILE
    tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 fi
-cd $WORKSPACE/
+cd $WORKSPACE/$NAME-$VERSION
 ./configure --prefix $SOFT_DIR
 make -j 8
