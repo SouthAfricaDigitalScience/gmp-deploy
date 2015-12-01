ab4b3080ea88e96fbdab2df9022c20ce52cc1c76
Bruce Becker <brucellino@gmail.com>
2015-12-01 10:41:11 +0100

    Added a deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+# this should be run after check-build finishes.
+. /etc/profile.d/modules.sh
+echo ${SOFT_DIR}
+module add deploy
+echo ${SOFT_DIR}
+cd ${WORKSPACE}/${NAME}-${VERSION}
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+./configure --with-gnu-ld --prefix $SOFT_DIR
+make install
+DIRS=`ls $SOFT_DIR`
+
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/gmp-deploy"
+setenv       GMP_VERSION       $VERSION
+setenv       GMP_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
+MODULE_FILE
+) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
