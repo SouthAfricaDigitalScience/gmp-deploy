6d5c5eec41d3725f5a01711c0b53de639bc31f60
Bruce Becker <brucellino@gmail.com>
2017-05-04 12:04:18 +0200

    Use the GMP_DIR env as defined in the modulefile instead of shell

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ setenv GMP_VERSION       $VERSION
 setenv GMP_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
 prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
 prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
-prepend-path CFLAGS            "-I${GMP_DIR}/include"
-prepend-path LDFLAGS           "-L${GMP_DIR}/lib"
+prepend-path CFLAGS            "-I$::env(GMP_DIR)/include"
+prepend-path LDFLAGS           "-L::env(GMP_DIR)/lib"
 MODULE_FILE
 ) > ${LIBRARIES}/${NAME}/${VERSION}
