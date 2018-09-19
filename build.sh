af891fd1504cc29550a34c3529746159ff4f7d5f
Bruce Becker <brucellino@gmail.com>
2018-09-19 10:01:12 +0200

    Fix Shellcheck errors

--- build.sh
+++ build.sh
@@ -18,35 +18,14 @@ module avail
 module add ci
 SOURCE_FILE=${NAME}-${VERSION}.tar.bz2
 whoami
-echo "REPO_DIR is "
-echo $REPO_DIR
-echo "SRC_DIR is "
-echo $SRC_DIR
-echo "WORKSPACE is "
-echo $WORKSPACE
-echo "SOFT_DIR is"
-echo $SOFT_DIR
+echo "REPO_DIR is $REPO_DIR"
+echo "SRC_DIR is $SRC_DIR"
+echo "WORKSPACE is $WORKSPACE"
+echo "SOFT_DIR is $SOFT_DIR"
 
-#  Download the source file
-
-# if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
-#   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
-#   echo "seems like this is the first build - let's get the source"
-#   wget https://gmplib.org/download/gmp/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
-#   echo "releasing lock"
-#   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
-# elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
-#   # Someone else has the file, wait till it's released
-#   while [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; do
-#     echo " There seems to be a download currently under way, will check again in 5 sec"
-#     sleep 5
-#   done
-# else
-#   echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
-# fi
-tar xjf  ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
-mkdir -vp ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
-cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+tar xjf  "${SRC_DIR}/${SOURCE_FILE}" -C "${WORKSPACE}" --skip-old-files
+mkdir -vp "${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}"
+cd "${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}"
 ../configure ABI=64 \
 --with-gnu-ld \
 --enable-static=yes \
@@ -55,5 +34,5 @@ cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
 --host=x86_64-pc-linux-gnu \
 --enable-cxx=yes \
 --enable-fat \
---prefix=${SOFT_DIR}
+--prefix="${SOFT_DIR}"
 make -j2
