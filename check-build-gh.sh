ff0237a032046a49d457d0956070f0b19f6284e2
Bruce Becker <brucellino@protonmail.com>
2023-04-10 10:25:16 +0200

    build: add check-build script to install artifacts in target (#197)
    build: add check-build script to install artifacts in target
    ci: add check build stage and artifact upload stage
    ci: ensure that release depends on build
    ci: run check install as root
    chore: add initial configuration of releaserc
    chore: experiment to find the build path
    ci: fix path to built libs when uploading artifact
    ci: preserve environment when running sudo
    ci: reduce scope of artifacts to store
    build: remove unbound LIBRARIES variable
    build: fix path to modulefile by adding modules subdir
    ci: check output of modulefile
    ci: add cache for installed artifacts
    build: add a glob to match everything under REPO_DIR
    ci: upload a directory instead of the entire frikkin filesystem
    fix: use REPO_DIR for deploy dir
    ci: experiment using line breaking in yaml for artifact path
    ci: experiment using traling slash
    ci: use a tarball instead of the actual path
    ci: fix REPO_DIR -> SOFT_DIR
    chore: add install artifacts to release assets
    
    ---------
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

---
+++ check-build-gh.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eou pipefail
+# Copyright 2023 Bruce Becker
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd "${PWD}/${NAME}-${VERSION}/build-${GITHUB_RUN_ID}"
+
+echo $?
+
+make install
+mkdir -vp "${SOFT_DIR}/libraries/${NAME}/modules"
+cat <<MODULE_FILE >"${SOFT_DIR}/libraries/${NAME}/modules/${VERSION}"
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       GMP_VERSION       $VERSION
+setenv       GMP_DIR           ${SOFT_DIR}/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
+prepend-path CFLAGS            "-I$::env(GMP_DIR)/include"
+prepend-path LDFLAGS           "-L$::env(GMP_DIR)/lib"
+MODULE_FILE
+cat "${SOFT_DIR}/libraries/${NAME}/modules/${VERSION}"
