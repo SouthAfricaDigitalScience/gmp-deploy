53532cb7073e020bf061b829ec45b28de36b17f4
Bruce Becker <brucellino@protonmail.com>
2023-04-10 09:42:00 +0200

    fix: use REPO_DIR for deploy dir
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- check-build-gh.sh
+++ check-build-gh.sh
@@ -31,7 +31,7 @@ proc ModulesHelp { } {
 
 module-whatis   "$NAME $VERSION."
 setenv       GMP_VERSION       $VERSION
-setenv       GMP_DIR           /data/ci-build/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+setenv       GMP_DIR           ${REPO_DIR}/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
 prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
 prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
 prepend-path CFLAGS            "-I$::env(GMP_DIR)/include"
